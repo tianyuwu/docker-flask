92456d355459001e5fd0d0dded512a04166205ce
tianyuwu <iwhitney@163.com>
2016-09-20 16:08:52 +0800

    blueprint test

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask
 from redis import Redis
+from werkzeug.routing import BaseConverter
+from flask import session, redirect, url_for, escape, request
+from blueprint import simple_page
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+app.secret_key = 'some_secret'
+
+# app.register_blueprint(simple_page)
+# Blueprint can be registered many times
+app.register_blueprint(simple_page, url_prefix='/pages')
+
+
+
+# app = Flask(__name__)
+# redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello world! I have been seen %s times.' % redis.get('hits')
+    # redis.incr('hits')
+    return 'Hello! I have been seen %s times.' % 1
 
-@app.route('/item/<id>')
+@app.route('/item/<id>/')
 def item(id):
 	return 'this is {}'.format(id)
  
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
---
+++ blueprint.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, abort, flash
+from jinja2 import TemplateNotFound
+
+simple_page = Blueprint('simple_page', __name__,
+                        template_folder='templates')
+
+
+@simple_page.route('/', defaults={'page': 'index'})
+@simple_page.route('/<page>')
+def show(page):
+    try:
+    	flash('You were successfully logged in')
+        return render_template('pages/%s.html' % page)
+    except TemplateNotFound:
+        abort(404)
